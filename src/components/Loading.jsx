92217bc882fb00ad0136414b477ddfae59c09f41
Mr.Jerrish <sheikmohamedanzarm.@gmail.com>
2026-05-14 17:36:56 +0530

    design for loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import Logo2 from "../assets/images/Logo3.png";
+import Logo from "../assets/images/Sheik1.png";
 const Loading = ({ loading = false, text = "Loading Please Wait..." }) => {
   if (!loading) return null;
 
@@ -13,8 +13,13 @@ const Loading = ({ loading = false, text = "Loading Please Wait..." }) => {
         bg-white dark:bg-slate-900
         px-6 py-4 rounded-xl shadow-xl"
       >
-        <div className=" bg-slate-100 borderborder-blue-400/40 rounded-full p-2 shadow-[0_0_20px_rgba(59,130,246,0.6)]   logo-loader ">
-          <img src={Logo2} className="w-10 h-10" alt="Loading" />
+        <div className=" bg-slate-100 borderborder-blue-400/40 rounded-full  shadow-[0_0_20px_rgba(59,130,246,0.6)] border border-blue-600  logo-loader ">
+          {/* <img src={Logo} className="w-10 h-10" alt="Loading" /> */}
+          <img
+            src={Logo}
+            className="w-16 h-16 object-cover rounded-full"
+            alt="Loading"
+          />
         </div>
 
         {/* Bouncing Dots */}
